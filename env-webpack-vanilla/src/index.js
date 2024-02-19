ee70b03aded8fe852496d24b38f7c71293d56e79
Peaumm <maxence.fouquet@gmail.com>
2024-02-19 17:59:30 +0100

    fix body

--- env-webpack-vanilla/src/index.js
+++ env-webpack-vanilla/src/index.js
@@ -1,14 +1,10 @@
 import Router from './router';
-import Chat from './controllers/Chat';
 import Bot from './controllers/Bot';
 import Bouton from './controllers/Bouton';
 
 import './index.scss';
 
 const routes = [{
-  url: '/chat',
-  controller: Chat
-}, {
   url: '/bot',
   controller: Bot
 }, {
