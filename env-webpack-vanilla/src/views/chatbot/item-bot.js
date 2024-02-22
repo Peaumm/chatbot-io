9caaa9b0eca1305a8b4a1149704051019cd86e8f
DavidChatton <davidchatton.dev@gmail.com>
2024-02-22 15:43:45 +0100

    fix css user bot add feature notification

--- env-webpack-vanilla/src/views/chatbot/item-bot.js
+++ env-webpack-vanilla/src/views/chatbot/item-bot.js
@@ -1,4 +1,4 @@
-export default () => (`
+export default (id) => (`
   <div class="bot d-flex align-items-center my-3">
     <div class="icon-robot-list rounded-circle d-flex align-items-center justify-content-center"> 
       <i class="fa-solid fa-robot"></i>
@@ -6,7 +6,7 @@ export default () => (`
     <div class="mx-2">
       <h3 class="fw-bold text-light mb-0">Echo</h3>
     </div>
-    <div class="notification rounded-circle d-flex justify-content-center">
+    <div id="${id}" class="notification rounded-circle d-flex justify-content-center align-items-center">
       <p>1</p>
     </div>
   </div>
